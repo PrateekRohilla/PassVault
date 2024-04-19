3dd16c933fb67804280965a49aaba6c9d70c9570
Prateek Rohilla <prateek98rohilla@gmail.com>
2024-04-19 14:21:49 +0530

    inputs fix & icons added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,9 +5,9 @@ const Navbar = () => {
     <nav className='bg-slate-800 text-white'>
         <div className='mycontainer flex justify-between items-center px-4 py-5 h-14'>
           <div className='logo font-bold text-2xl'>
-            <span className='text-green-700'>&lt;</span>
+            <span className='text-green-500'>&lt;</span>
             PassVault
-            <span className='text-green-700'>/&gt;</span>
+            <span className='text-green-500'>/&gt;</span>
           </div>
               <ul>
                   <li className='flex gap-4'>
