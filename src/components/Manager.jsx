17750e11d56d50bcab0692d8c30ca5595cd685d7
Prateek Rohilla <prateek98rohilla@gmail.com>
2024-04-19 23:19:20 +0530

    display saved credentials

--- src/components/Manager.jsx
+++ src/components/Manager.jsx
@@ -1,22 +1,22 @@
-import React, { useRef,useState,useEffect } from 'react'
+import React, { useRef, useState, useEffect } from 'react'
 
 const Manager = () => {
 
   const ref = useRef()
-  const [form, setform] = useState({site: "", username: "", password: ""})
+  const [form, setform] = useState({ site: "", username: "", password: "" })
   const [passwordArray, setPasswordArray] = useState([])
 
   useEffect(() => {
     let passwords = localStorage.getItem("passwords")
-    
-    if(passwords)
+
+    if (passwords)
       setPasswordArray(JSON.parse(passwords))
 
   }, [])
-  
+
 
   const showPassword = () => {
-    if(ref.current.src.includes("icons/eyecross.png"))
+    if (ref.current.src.includes("icons/eyecross.png"))
       ref.current.src = "icons/eye.png"
     else
       ref.current.src = "icons/eyecross.png"
@@ -27,12 +27,12 @@ const Manager = () => {
     localStorage.setItem("passwords", JSON.stringify([...passwordArray, form]))
     console.log([...passwordArray, form]);
   }
-  
+
   const handleChange = (e) => {
-    setform({...form, [e.target.name]: e.target.value})
+    setform({ ...form, [e.target.name]: e.target.value })
   }
-  
-  
+
+
   return (
     <>
       <div className="absolute inset-0 -z-10 h-full w-full bg-green-50 bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]"><div className="absolute left-0 right-0 top-0 -z-10 m-auto h-[310px] w-[310px] rounded-full bg-fuchsia-400 opacity-20 blur-[100px]"></div></div>
@@ -64,6 +64,31 @@ const Manager = () => {
             </lord-icon>
             Add Password
           </button>
+        </div>
+        <div className="passwords">
+          <h2 className='font-bold text-2xl py-4'>Your Passwords</h2>
+          {passwordArray.length === 0 && <div>No Passwords to show</div>}
+          {passwordArray.length != 0 &&
+            <table className="table-auto w-full rounded-md overflow-hidden">
+              <thead className='bg-green-800 text-white'>
+                <tr>
+                  <th className='py-2'>Site</th>
+                  <th className='py-2'>Username</th>
+                  <th className='py-2'>Password</th>
+                </tr>
+              </thead>
+              <tbody className='bg-green-100'>
+                {
+                  passwordArray.map((item, index) => {
+                    return <tr key={index}>
+                      <td className='py-2 border border-white text-center w-32'><a href={item.site} target="_blank">{item.site}</a></td>
+                      <td className='py-2 border border-white text-center w-32'>{item.username}</td>
+                      <td className='py-2 border border-white text-center w-32'>{item.password}</td>
+                    </tr>
+                  })
+                }
+              </tbody>
+            </table>}
         </div>
       </div>
     </>
