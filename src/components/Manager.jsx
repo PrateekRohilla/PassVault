513849afd104c0928fadec69a5daf96dfaf624bd
Prateek Rohilla <prateek98rohilla@gmail.com>
2024-04-21 17:55:31 +0530

    Responsive design updated

--- src/components/Manager.jsx
+++ src/components/Manager.jsx
@@ -1,6 +1,4 @@
 import React, { useRef, useState, useEffect } from 'react'
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
 import { v4 as uuidv4 } from 'uuid';
 
 const Manager = () => {
@@ -18,20 +16,6 @@ const Manager = () => {
 
   }, [])
 
-  const copyText = (text) => {
-    toast('Copied to clipboard!', {
-      position: "top-right",
-      autoClose: 5000,
-      hideProgressBar: false,
-      closeOnClick: true,
-      pauseOnHover: true,
-      draggable: true,
-      progress: undefined,
-      theme: "dark",
-    });
-    navigator.clipboard.writeText(text)
-  }
-
   const showPassword = () => {
     passwordRef.current.type = "text"
     if (ref.current.src.includes("icons/eyecross.png")) {
@@ -45,16 +29,21 @@ const Manager = () => {
   }
 
   const savePassword = () => {
-    setPasswordArray([...passwordArray, { ...form, id: uuidv4() }])
-    localStorage.setItem("passwords", JSON.stringify([...passwordArray, { ...form, id: uuidv4() }]))
-    setform({site: "", username: "", password: ""})
+    if (form.site.length > 3 && form.username.length > 3 && form.password.length > 3) {
+      setPasswordArray([...passwordArray, { ...form, id: uuidv4() }])
+      localStorage.setItem("passwords", JSON.stringify([...passwordArray, { ...form, id: uuidv4() }]))
+      setform({ site: "", username: "", password: "" })
+    }
+    else{
+      alert("Password not saved\nEnter correct details");
+    }
   }
 
   const deletePassword = (id) => {
     let c = confirm("Do you really want to delete this Password?")
-    if(c){
-      setPasswordArray(passwordArray.filter(item=>item.id!==id))
-      localStorage.setItem("passwords", JSON.stringify(passwordArray.filter(item=>item.id!==id)))
+    if (c) {
+      setPasswordArray(passwordArray.filter(item => item.id !== id))
+      localStorage.setItem("passwords", JSON.stringify(passwordArray.filter(item => item.id !== id)))
     }
   }
 
@@ -71,25 +60,8 @@ const Manager = () => {
 
   return (
     <>
-
-      <ToastContainer
-        position="top-right"
-        autoClose={5000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-        theme="light"
-        transition="Bounce"
-      />
-      {/* Same as */}
-      <ToastContainer />
-
       <div className="absolute inset-0 -z-10 h-full w-full bg-green-50 bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]"><div className="absolute left-0 right-0 top-0 -z-10 m-auto h-[310px] w-[310px] rounded-full bg-fuchsia-400 opacity-20 blur-[100px]"></div></div>
-      <div className='mycontainer'>
+      <div className='pt-3 md:mycontainer'>
         <h1 className='text-4xl font-bold text-center'>
           <span className='text-green-500'>&lt;</span>
           PassVault
@@ -99,11 +71,11 @@ const Manager = () => {
           Your own Password Manager
         </p>
         <div className='flex flex-col p-4 text-black gap-8 items-center'>
-          <input value={form.site} onChange={handleChange} placeholder='Enter website URL' className='rounded-full border border-green-500 w-full p-4 py-1' type="text" name="site" id="" />
-          <div className='flex w-full justify-between gap-8'>
-            <input value={form.username} onChange={handleChange} placeholder='Enter Username' className='rounded-full border border-green-500 w-full p-4 py-1' type="text" name="username" id="" />
+          <input value={form.site} onChange={handleChange} placeholder='Enter website URL' className='rounded-full border border-green-500 w-full p-4 py-1' type="text" name="site" id="site" />
+          <div className='flex flex-col md:flex-row w-full justify-between gap-8'>
+            <input value={form.username} onChange={handleChange} placeholder='Enter Username' className='rounded-full border border-green-500 w-full p-4 py-1' type="text" name="username" id="username" />
             <div className="relative">
-              <input ref={passwordRef} value={form.password} onChange={handleChange} placeholder='Enter Password' className='rounded-full border border-green-500 w-full p-4 py-1' type="password" name="password" id="" />
+              <input ref={passwordRef} value={form.password} onChange={handleChange} placeholder='Enter Password' className='rounded-full border border-green-500 w-full p-4 py-1' type="password" name="password" id="password" />
               <span className='absolute right-[3px] top-[4px] cursor-pointer' onClick={showPassword}>
                 <img ref={ref} className='p-1' width={26} src="icons/eye.png" alt="eye" />
               </span>
