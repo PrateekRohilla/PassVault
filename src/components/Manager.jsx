12cd84ec548b322a1a1090e849978572fda6acfe
Prateek Rohilla <prateek98rohilla@gmail.com>
2024-05-02 17:32:41 +0530

    UI fix

--- src/components/Manager.jsx
+++ src/components/Manager.jsx
@@ -16,6 +16,10 @@ const Manager = () => {
 
   }, [])
 
+  const copyText = (text) => {
+    navigator.clipboard.writeText(text)
+}
+
   const showPassword = () => {
     passwordRef.current.type = "text"
     if (ref.current.src.includes("icons/eyecross.png")) {
@@ -60,29 +64,29 @@ const Manager = () => {
 
   return (
     <>
-      <div className="absolute inset-0 -z-10 h-full w-full bg-green-50 bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]"><div className="absolute left-0 right-0 top-0 -z-10 m-auto h-[310px] w-[310px] rounded-full bg-fuchsia-400 opacity-20 blur-[100px]"></div></div>
+      <div className="absolute inset-0 -z-10 h-full w-full bg-gray-50 bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]"><div className="absolute left-0 right-0 top-0 -z-10 m-auto h-[310px] w-[310px] rounded-full bg-fuchsia-400 opacity-20 blur-[100px]"></div></div>
       <div className='pt-3 md:mycontainer'>
         <h1 className='text-4xl font-bold text-center'>
-          <span className='text-green-500'>&lt;</span>
+          <span className='text-gray-500'>&lt;</span>
           PassVault
-          <span className='text-green-500'>/&gt;</span>
+          <span className='text-gray-500'>/&gt;</span>
         </h1>
-        <p className='text-green-900 text-lg text-center'>
+        <p className='text-gray-900 text-lg text-center'>
           Your own Password Manager
         </p>
-        <div className='flex flex-col p-4 text-black gap-8 items-center'>
-          <input value={form.site} onChange={handleChange} placeholder='Enter website URL' className='rounded-full border border-green-500 w-full p-4 py-1' type="text" name="site" id="site" />
+        <div className='flex flex-col p-4 text-gray gap-8 items-center'>
+          <input value={form.site} onChange={handleChange} placeholder='Enter website URL' className='rounded-full border border-gray-500 w-full p-4 py-1' type="text" name="site" id="site" />
           <div className='flex flex-col md:flex-row w-full justify-between gap-8'>
-            <input value={form.username} onChange={handleChange} placeholder='Enter Username' className='rounded-full border border-green-500 w-full p-4 py-1' type="text" name="username" id="username" />
+            <input value={form.username} onChange={handleChange} placeholder='Enter Username' className='rounded-full border border-gray-500 w-full p-4 py-1' type="text" name="username" id="username" />
             <div className="relative">
-              <input ref={passwordRef} value={form.password} onChange={handleChange} placeholder='Enter Password' className='rounded-full border border-green-500 w-full p-4 py-1' type="password" name="password" id="password" />
+              <input ref={passwordRef} value={form.password} onChange={handleChange} placeholder='Enter Password' className='rounded-full border border-gray-500 w-full p-4 py-1' type="password" name="password" id="password" />
               <span className='absolute right-[3px] top-[4px] cursor-pointer' onClick={showPassword}>
                 <img ref={ref} className='p-1' width={26} src="icons/eye.png" alt="eye" />
               </span>
             </div>
 
           </div>
-          <button onClick={savePassword} className='flex justify-center items-center gap-2 bg-green-400 hover:bg-green-300 rounded-full px-8 py-2 w-fit border border-green-900'>
+          <button onClick={savePassword} className='flex justify-center items-center gap-2 bg-gray-400 hover:bg-gray-300 rounded-full px-8 py-2 w-fit border border-gray-900'>
             <lord-icon
               src="https://cdn.lordicon.com/jgnvfzqg.json"
               trigger="hover">
@@ -95,7 +99,7 @@ const Manager = () => {
           {passwordArray.length === 0 && <div>No Passwords to show</div>}
           {passwordArray.length != 0 &&
             <table className="table-auto w-full rounded-md overflow-hidden">
-              <thead className='bg-green-800 text-white'>
+              <thead className='bg-gray-800 text-white'>
                 <tr>
                   <th className='py-2'>Site</th>
                   <th className='py-2'>Username</th>
@@ -103,7 +107,7 @@ const Manager = () => {
                   <th className='py-2'>Actions</th>
                 </tr>
               </thead>
-              <tbody className='bg-green-100'>
+              <tbody className='bg-gray-100'>
                 {
                   passwordArray.map((item, index) => {
                     return <tr key={index}>
